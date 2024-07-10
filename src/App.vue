4c873618825b4ac6795b20a3f075bfe671b50c00
Mony <mony_hill@qq.com>
2024-07-11 04:44:30 +0800

    feat: 更新登录后用户界面的大体框架，放入了很多空页面待完成

--- src/App.vue
+++ src/App.vue
@@ -1,60 +1,8 @@
 <script setup lang="ts">
-// import { RouterLink, RouterView } from 'vue-router'
-// import HelloWorld from './components/HelloWorld.vue'
-import { ref } from "vue";
-import axios from "axios";
-
-interface AdminInfo {
-  id: string,
-  name: string,
-  password: string,
-  level: number,
-}
-
-const infos = ref<[AdminInfo]>();
-
-const update = async () => {
-  await axios
-  .get(`/api/test`)
-  .then(response => {
-    if (response.status === 200) {
-      infos.value = response.data;
-    }
-  })
-  .catch(function (error) {
-    console.log(error);
-  });
-}
 </script>
 
 <template>
-  <div>
-    <el-button type="primary" @click="update" round>更新</el-button>
-    <div v-for="(info, index) in infos" :key="index">
-      <h1>管理员 {{ index }} 号</h1>
-      <p>账号 ID：{{ info.id }}</p>
-      <p>账号名字：{{ info.name }}</p>
-      <p>账号密码：{{ info.password }}</p>
-      <p>（非实际业务逻辑中的密码）</p>
-      <p>权限等级：{{ info.level }}</p>
-    </div>
-  </div>
-  <!--
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
-  </header>
-
   <RouterView />
--->
 </template>
 
 <style scoped>
---
+++ src/views/Layout/components/FixedLayout.vue
@@ -0,0 +1,38 @@
+<script setup lang="ts">
+import NavLayout from './NavLayout.vue'
+import { useWindowScroll } from '@vueuse/core'
+const { x, y } = useWindowScroll();
+</script>
+
+<template>
+  <div class="stiky-header" :class="{ show: y > 78 }">
+    <NavLayout />
+  </div>
+</template>
+
+<style scoped>
+.stiky-header {
+  width: 100%;
+  height: 80px;
+  position: fixed;
+  left: 0;
+  top: 0;
+  z-index: 100;
+  display: flex;
+  align-items: center;
+  justify-content: flex-end;
+  background-color: var(--color-background);
+  border-bottom: 2px solid var(--color-border);
+
+  /* 页面处于最顶端时，该组件向上平移并完全透明 */
+  transform: translateY(-100%);
+  opacity: 0;
+
+  /* 页面向下移动一定距离后，取消平移和透明 */
+  &.show {
+    transition: all 0.3s linear;
+    transform: none;
+    opacity: 1;
+  }
+}
+</style>
