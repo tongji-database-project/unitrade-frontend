cf8859af4c6135cd82458181c0f91911ec006ba6
flameagain <3110630186@qq.com>
2024-07-11 20:52:56 +0800

    update
    更新了用户登录注册界面

--- src/views/Login/LoginView.vue
+++ src/views/Login/LoginView.vue
@@ -13,14 +13,52 @@ function chooseEnroll(){
 </script>
 
 <template>
-  <main>
-    <LoginForm v-if="isLogin" />
-    <EnrollForm v-else />
-    <p @click="chooseLogin">登录</p>
-    <p @click="chooseEnroll">注册</p>
+  <header class="head">
+    <RouterLink class="logo" to="/">
+        <!-- TODO: logo 待替换 -->
+        <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="50" height="50" />
+    </RouterLink>
+  </header>
+ 
+  <main class="main-container">
+    <div>
+      <LoginForm v-if="isLogin" />
+      <EnrollForm v-else />
+    </div>
+    <div>
+      <el-space class="container" spacer="|">
+        <p class="login-container" @click="chooseLogin">登录</p>
+        <p class="enroll-container" @click="chooseEnroll">注册</p>
+    </el-space>
+    </div>
   </main>
 </template>
 
 <style scoped>
-
+  .head{
+    display: flex;
+    position: absolute;
+    top: 10px;
+    left: 10px;
+  }
+  .main-container{
+    display: flex;
+    flex-direction: column;
+    align-items: center; 
+    justify-content: center;
+  }
+  .main-container div { 
+    margin-bottom: 10px;
+  }
+  .container{
+    flex-direction: row;
+    padding: 0.5rem;
+  }
+  .login-container, .enroll-container{
+    cursor: pointer;
+    transition: transform 0.3s ease;
+  }
+  .login-container:hover , .enroll-container:hover {
+    transform: scale(1.25); 
+  }
 </style>
--- src/views/Login/components/LoginForm.vue
+++ src/views/Login/components/LoginForm.vue
@@ -5,19 +5,40 @@ function submitForm(){
 </script>
 
 <template>
-  <form @submit.prevent="submitForm">
-      <div>
-        <label for="username">用户名:</label>
-        <input type="text" id="username" required>
+  <form @submit.prevent="submitForm" class="form-container">
+      <div class="input-container">
+        <input type="text" id="username" placeholder="请输入用户名" required>
+      </div>
+      <div class="input-container">
+        <input type="password" id="password" placeholder="请输入密码" required>
       </div>
       <div>
-        <label for="password">密码:</label>
-        <input type="password" id="password" required>
+        <button type="submit">提交</button>
       </div>
-      <button type="submit">提交</button>
     </form>
 </template>
 
 <style scoped>
-  
+  .form-container{
+    display: flex;
+    flex-direction: column;
+    align-items: center; 
+  } 
+  .form-container div { 
+    margin-bottom: 20px;
+  }
+  .input-container input{
+    width: 300px;
+    height: 30px;
+  }
+  button[type="submit"] {
+    width: 200px;
+    height: 30px;
+    cursor: pointer;
+    transition: transform 0.3s ease, box-shadow 0.3s ease;
+  }
+  button[type="submit"]:hover {
+    transform: scale(1.1); 
+    box-shadow: 0 8px 16px rgba(0, 0, 0, 0.3); 
+  }
 </style>
