a7f3cd1496f39a9aed93d56c27de125db15a2ce6
h087612 <1710587218@qq.com>
2024-07-11 14:46:17 +0800

    fix: 去除不必要更改

--- src/main.ts
+++ src/main.ts
@@ -4,14 +4,12 @@ import { createApp } from "vue";
 import { createPinia } from "pinia";
 import axios from "axios";
 import VueAxios from "vue-axios";
-import ElementPlus from 'element-plus'
-import 'element-plus/dist/index.css'
 
 import App from "./App.vue";
 import router from "./router";
 
 const app = createApp(App)
-app.use(ElementPlus);
+
 app.use(createPinia());
 app.use(router);
 app.use(VueAxios, axios);
--- src/router/index.ts
+++ src/router/index.ts
@@ -95,11 +95,7 @@ const router = createRouter({
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
       component: () => import('../views/AboutView.vue')
-    },
-    { path: '/', component: Dashboard },
-    { path: '/products', component: ProductManagement },
-    { path: '/orders', component: OrderManagement },
-    { path: '/profile', component: ProfileSettings }
+    }
   ]
 })
 
