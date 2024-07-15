cae5ed14003a45fc90c0c6bcfcddc26e441d8a38
h087612 <1710587218@qq.com>
2024-07-15 20:32:18 +0800

    feat: 添加商品详情界面和退款界面路由

--- src/views/Order/OrderView.vue
+++ src/views/Order/OrderView.vue
@@ -1,7 +1,16 @@
 <template>
   <!-- TODO: 待完成 -->
   <h1>订单详情页面（WIP）</h1>
-  <div v-for="i in 30" :key="i" style="padding-top: 1rem;">
-    填充内容，用于拉长页面长度
+  <div>
+  <h1>订单详情</h1>
+    <p>订单 ID: {{ orderId }}</p>
+    <!-- 其他订单详情 -->
   </div>
 </template>
+
+<script lang="ts" setup>
+import { useRoute } from 'vue-router'
+
+const route = useRoute()
+const orderId = route.params.id
+</script>
--- src/views/RefundRequest/RefundRequestView.vue
+++ src/views/RefundRequest/RefundRequestView.vue
@@ -1,7 +1,16 @@
 <template>
   <!-- TODO: 待完成 -->
   <h1>退款申请页面（WIP）</h1>
-  <div v-for="i in 30" :key="i" style="padding-top: 1rem;">
-    填充内容，用于拉长页面长度
+  <div>
+    <h1>申请退款</h1>
+    <p>订单 ID: {{ orderId }}</p>
+    <!-- 退款表单 -->
   </div>
 </template>
+
+<script lang="ts" setup>
+import { useRoute } from 'vue-router'
+
+const route = useRoute()
+const orderId = route.params.id
+</script>
