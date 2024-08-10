42d891b196c471ef521450e0c4893d8c45b4a560
CChris152 <1650306859@qq.com>
2024-08-11 02:18:44 +0800

    add 管理员推送功能组件前端基础完成

---
+++ src/views/Admin/components/OneRecommand.vue
@@ -0,0 +1,62 @@
+<!-- 推送 -->
+<script lang="ts" setup>
+  const props=defineProps({
+    introduce:{
+      type:String,
+      default:"",
+    },
+    num:{
+      type:Number,
+      default:0,
+    }
+  })
+
+  const emit = defineEmits(['delete'])
+  const deleteconfirm = () => {
+    emit('delete', props.num);
+  }
+</script>
+
+<template>
+  <div class="main">
+    <div class="introduce">
+      {{ introduce }}
+    </div>
+    <div @click="deleteconfirm" class="button">
+      同意推送
+    </div>
+    <div @click="deleteconfirm" class="button">
+      拒绝推送
+    </div>
+  </div>
+</template>
+
+<style scoped>
+  .main{
+    height: 200px; 
+    background-color:antiquewhite;
+    margin-bottom: 20px;
+  }
+
+  .introduce{
+    font-weight: bold;  
+    font-size: 20px; 
+    margin-left: 7%;
+    margin-top: 2%;
+  }
+
+  .button{
+    width: 100px;
+    height: 30px;
+    border: 2px solid black;
+    background-color: aqua;
+    transition: transform 0.3s ease;
+    text-align: center;  
+    margin-top: 3%;
+    margin-left: 5%;
+  }
+
+  .button:hover{
+    transform: scale(1.1);
+  }
+</style> 
--- src/views/Admin/components/RecommandSection.vue
+++ src/views/Admin/components/RecommandSection.vue
@@ -1,10 +1,26 @@
+<!-- 推送 -->
+<script lang="ts" setup>
+  import { ref } from 'vue';  
+  import OneRecommand from './OneRecommand.vue';
+
+  let items=ref(["商品1","商品2","商品3","商品4","商品5","商品6"]);
+
+  const deleteconfirm = (index:number) => {
+    items.value.splice(index,1);
+  }
+</script>
+
 <template>
-  <div class="main">recommand</div>
+  <div class="main">
+    <div v-for="(item, index) in items" :key="index">
+      <OneRecommand :introduce="item" :num="index" @delete="deleteconfirm"/>
+    </div>
+  </div>
 </template>
 
 <style scoped>
   .main{
-    height: 600px;
-    background-color:antiquewhite;
+    max-height: 600px; 
+    overflow-y: auto;
   }
 </style> 
