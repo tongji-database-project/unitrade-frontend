c442b4f1e2f43c6b29ff6099a3d022a30d8b1ddb
flameagain <3110630186@qq.com>
2024-08-21 00:50:44 +0800

    feat 退款组件细化

---
+++ src/views/Admin/components/OneRefund.vue
@@ -0,0 +1,82 @@
+<!-- 退款 -->
+<script lang="ts" setup>
+    import type { refundinformation } from './RefundSection.vue';
+
+    const props=defineProps({
+    onerefundinformation:{
+        type:Object as () => refundinformation,
+        default:null,
+    },
+    num:{
+        type:Number,
+        default:0,
+    }
+    })
+
+    const emit = defineEmits(['delete'])
+    const deleteconfirm = () => {
+        emit('delete', props.num);
+    }
+
+    function refundjudge(isagree:boolean){
+        deleteconfirm();
+    }
+
+</script>
+
+<template>
+    <div calss="main">
+        <div class="introduce">
+            商家：{{ onerefundinformation.seller }}
+        </div>
+        <div class="introduce">
+            买家：{{ onerefundinformation.buyer }}
+        </div>
+        <div class="introduce">
+            商品名：{{ onerefundinformation.commodity }}
+        </div>
+        <div class="introduce">
+            申请时间：{{ onerefundinformation.time }}
+        </div>
+        <div @click="refundjudge(true)" class="button">
+            同意退款
+        </div>
+        <div @click="refundjudge(false)" class="button">
+            拒绝退款
+        </div>
+    </div>
+</template>
+
+<style scoped>
+ .main{
+    height: 250px; 
+    background-color:rgb(246, 246, 246);
+    margin-bottom: 10px;
+    border: 2px solid #a4a4a4b7;
+    border-radius: 15px;
+  }
+
+  .introduce{
+    font-weight: bold;  
+    font-size: 15px; 
+    margin-left: 5%;
+    margin-top: 1.5%;
+  }
+
+  .button{
+    width: 100px;
+    height: 30px;
+    border: 2px solid black;
+    background-color: rgb(220, 253, 253);
+    transition: transform 0.3s ease;
+    text-align: center;  
+    margin-top: 1.5%;
+    margin-left: 5%;
+    display: inline-block;
+    border-radius: 5px;
+  }
+
+  .button:hover{
+    transform: scale(1.1);
+  }
+</style>
--- src/views/Admin/components/RefundSection.vue
+++ src/views/Admin/components/RefundSection.vue
@@ -1,10 +1,44 @@
+<!-- 退款 -->
+<script lang="ts" setup>
+  import { ref } from 'vue';  
+  import OneRefund from './OneRefund.vue';
+
+  export type refundinformation={
+    seller:string;
+    buyer:string;
+    commodity:string;
+    time:Date;
+  }
+
+  let refundinformations = ref<refundinformation[]>([]);
+  refundinformations.value.push({seller:"商家1",buyer:"买家1",commodity:"商品1",time:new Date('2023-03-21T14:30:00')});
+  refundinformations.value.push({seller:"商家2",buyer:"买家2",commodity:"商品2",time:new Date('2024-03-21T14:30:00')});
+  refundinformations.value.push({seller:"商家3",buyer:"买家3",commodity:"商品3",time:new Date('2025-03-21T14:30:00')});
+  refundinformations.value.push({seller:"商家4",buyer:"买家4",commodity:"商品4",time:new Date('2026-03-21T14:30:00')});
+
+  const deleteconfirm = (index:number) => {
+    refundinformations.value.splice(index,1);
+  }
+</script>
+
 <template>
-  <div class="main">refund</div>
+  <div class="main">
+    <div v-for="(item, index) in refundinformations" :key="index" class="table">
+      <OneRefund :onerefundinformation="item" :num="index" @delete="deleteconfirm"/>
+    </div>
+  </div>
 </template>
 
 <style scoped>
   .main{
-    height: 600px;
-    background-color: aliceblue;
+    max-height: 650px; 
+    overflow-y: auto;
+  }
+  .table{
+    background-color: rgb(255, 255, 255);
+    margin-bottom: 15px; /* 调整这个数值来增加或减少间距 */
+    padding: 10px; /* 可选：添加填充以让表格内容与边界有些距离 */
+    border: 1px solid #ddd; /* 可选：为表格添加边框 */
+    border-radius: 5px; /* 可选：为表格添加圆角 */
   }
 </style> 
