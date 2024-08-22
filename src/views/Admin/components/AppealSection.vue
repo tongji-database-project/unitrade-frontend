8a81736dd652fdefe0d7441781dd7fca0605203b
flameagain <3110630186@qq.com>
2024-08-22 17:39:01 +0800

    feat ：申诉组件细化

--- src/views/Admin/components/AppealSection.vue
+++ src/views/Admin/components/AppealSection.vue
@@ -1,11 +1,42 @@
+<!-- 投诉 -->
+<script lang="ts" setup>
+  import { ref } from 'vue';  
+  import OneAppeal from './OneAppeal.vue';
+
+  export type appealinformation={
+    seller:string;
+    credibility:number;
+    reason:string;
+    complainant:string;
+    time:Date;
+  }
+
+  let appealinformations = ref<appealinformation[]>([]);
+  appealinformations.value.push({seller:"商家1",credibility:100,reason:"无",complainant:"用户1",time:new Date('2023-03-21T14:30:00')});
+  appealinformations.value.push({seller:"商家2",credibility:100,reason:"无",complainant:"用户2",time:new Date('2024-03-21T14:30:00')});
+  appealinformations.value.push({seller:"商家3",credibility:100,reason:"无",complainant:"用户3",time:new Date('2025-03-21T14:30:00')});
+  appealinformations.value.push({seller:"商家4",credibility:100,reason:"无",complainant:"用户4",time:new Date('2026-03-21T14:30:00')});
+
+  const deleteconfirm = (index:number) => {
+    appealinformations.value.splice(index,1);
+  }
+</script>
 
 <template>
-  <div class="main">appeal</div>
+  <div class="main">
+    <div v-for="(item, index) in appealinformations" :key="index" class="table">
+      <OneAppeal :oneappealinformation="item" :num="index" @delete="deleteconfirm"/>
+    </div>
+  </div>
 </template>
 
 <style scoped>
   .main{
-    height: 600px;
-    background-color:bisque;
+    max-height: 650px; 
+    overflow-y: auto;
+  }
+
+  .table{
+    background-color: rgb(255, 255, 255);
   }
 </style> 
---
+++ src/views/Admin/components/OneAppeal.vue
@@ -0,0 +1,81 @@
+<!-- 申诉 -->
+<script lang="ts" setup>
+  import type { appealinformation } from './AppealSection.vue';
+
+  const props=defineProps({
+    oneappealinformation:{
+      type:Object as () => appealinformation,
+      default:null,
+    },
+    num:{
+      type:Number,
+      default:0,
+    }
+  })
+
+  const emit = defineEmits(['delete'])
+  const deleteconfirm = () => {
+    emit('delete', props.num);
+  }
+
+  function appealjudge(isagree:boolean){
+    deleteconfirm();
+  }
+</script>
+
+<template>
+  <div class="main">
+    <div class="introduce">
+      申诉人：{{ oneappealinformation.seller }}
+    </div>
+    <div class="introduce">
+      申诉原因：{{ oneappealinformation.reason }}
+    </div>
+    <div class="introduce">
+      投诉人：{{ oneappealinformation.complainant }}
+    </div>
+    <div class="introduce">
+      申诉时间：{{ oneappealinformation.time }}
+    </div>
+    <div @click="appealjudge(true)" class="button">
+      同意投诉
+    </div>
+    <div @click="appealjudge(false)" class="button">
+      拒绝投诉
+    </div>
+  </div>
+</template>
+
+<style scoped>
+  .main{
+    height: 250px; 
+    background-color:rgb(246, 246, 246);
+    margin-bottom: 10px;
+    border: 2px solid #a4a4a4b7;
+    border-radius: 15px;
+  }
+
+  .introduce{
+    font-weight: bold;  
+    font-size: 15px; 
+    margin-left: 5%;
+    margin-top: 1.5%;
+  }
+
+  .button{
+    width: 100px;
+    height: 30px;
+    border: 1.5px solid rgb(100, 100, 100);
+    background-color: #e9e9e9;;
+    color: rgb(0, 0, 0);
+    transition: transform 0.3s ease;
+    text-align: center;  
+    margin-top: 1.5%;
+    margin-left: 5%;
+    display: inline-block;
+  }
+
+  .button:hover{
+    transform: scale(1.1);
+  }
+</style> 
