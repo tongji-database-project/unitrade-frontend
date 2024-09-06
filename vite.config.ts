36a5c4d0f78ce9076f0e7228522aa3bb4ac90dba
Mony <mony_hill@qq.com>
2024-09-06 19:11:32 +0800

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -27,7 +27,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "http://localhost:5258",
+        target: "http://47.97.215.255",
         changeOrigin: true,
          //rewrite: (path) => path.replace(/^\/api/, ""),
       }
