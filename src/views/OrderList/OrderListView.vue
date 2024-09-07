b556cdca6a5dc487de9ff3dfecdc3964f33f78a8
h087612 <1710587218@qq.com>
2024-09-07 17:16:24 +0800

    fix: 显示商品名称和订单状态关键字

--- src/views/OrderList/OrderListView.vue
+++ src/views/OrderList/OrderListView.vue
@@ -1,5 +1,4 @@
 <template>
-  <!-- TODO: 待完成 -->
   <h1>全部订单</h1>
   <el-container class="myOrder" style="height: 500px">
     <el-container>
@@ -7,8 +6,13 @@
         <el-scrollbar>
           <el-table :data="tableData">
             <el-table-column prop="ordeR_ID" label="订单号" width="150" />
-            <el-table-column prop="merchandisE_ID" label="商品代码" width="300"/>
-            <el-table-column prop="state" label="订单状态" width="150" />
+            <el-table-column prop="merchandise_name" label="商品名称" width="300"/>
+            <el-table-column prop="state" label="订单状态" width="150">
+              <template #default="scope">
+                <!-- 根据 state 显示相应的文本 -->
+                {{ formatState(scope.row.state) }}
+              </template>
+            </el-table-column>
             <el-table-column prop="ordeR_TIME" label="下单时间" width="220" />
             <el-table-column prop="receivinG_TIME" label="收货时间" width="220" />
             <el-table-column prop="ordeR_QUANITY" label="订单数量" width="100"/>
@@ -30,7 +34,7 @@
 <script lang="ts" setup>
 import { ref,onMounted } from 'vue'
 import { useRouter } from 'vue-router'
-import { getOrderInfo,getUserAddress } from '@/apis/order';
+import { getOrderInfo,getUserAddress,getMerchandiseNameById } from '@/apis/order';
 import type { Order } from '@/utils/interfaces';
 const router = useRouter()
 // 定义包含多个商品的订单数据
@@ -41,12 +45,12 @@ const fetchOrders = async () => {
   try {
     // 调用 API 方法并传递查询参数
     const response = await getOrderInfo({
-      order_id: '',  // 如果你想要过滤指定的 order_id，可以在这里传递
-      state: '',  // 可以传递状态来筛选订单
-      merchandise: '',  // 传递商品名称
-      order_quantity: undefined,  // 可以根据数量筛选订单
-      order_time: undefined,  // 传递订单时间
-      receiving_time: undefined  // 传递收货时间
+      order_id: '',
+      state: '',
+      merchandise: '',
+      order_quantity: undefined,
+      order_time: undefined,
+      receiving_time: undefined
     });
 
     if (response) {
@@ -54,17 +58,21 @@ const fetchOrders = async () => {
       const address = await getUserAddress();
 
       if (address) {
-        // 为每个订单添加用户地址信息
-        tableData.value = response.map(order => ({
-          ...order,
-          userAddress: address  // 添加用户地址到每个订单数据对象中
+        // 为每个订单添加用户地址和商品名信息
+        const ordersWithDetails = await Promise.all(response.map(async (order) => {
+          const merchandise_name = await getMerchandiseNameById(order.merchandisE_ID);
+          return {
+            ...order,
+            userAddress: address,
+            merchandise_name:merchandise_name // 添加商品名到订单数据对象中
+          };
         }));
-        console.log('成功获取地址',address)
+
+        tableData.value = ordersWithDetails;
+        console.log('成功获取地址和商品名', tableData.value);
       } else {
         tableData.value = response; // 如果未获取到地址，只显示订单数据
       }
-
-      console.log('获取订单成功', tableData.value);
     } else {
       console.error('无法获取订单数据');
     }
@@ -91,6 +99,20 @@ const handleRefundClick = (order: any) => {
     params: { id: order.ordeR_ID }, 
     query: { order: encodedOrderData } 
   });
+};
+
+const statusMap: { [key: string]: string } = {
+  'ysh': '已收货',
+  'Rec': '已收货',
+  'yjs': '已拒收',
+  'Tra': '运输中',
+  'UNP': '未付款',
+  'WAI': '待发货'
+  // 添加更多状态
+};
+
+const formatState = (state: string) => {
+  return statusMap[state] || '未知状态';
 };
 
 // 页面加载时获取订单数据
